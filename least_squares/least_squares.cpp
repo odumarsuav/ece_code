7c0e8234aff18f35defa08bc1bdbc43d6a741350
odumarsuav <aglan001@odu.edu>
2015-03-18 10:12:51 -0400

    different approach using ellipse fit

--- least_squares/least_squares.cpp
+++ least_squares/least_squares.cpp
@@ -17,6 +17,9 @@ So for a simple RGB color image, HE should not be applied individually on each c
 using namespace std;
 using namespace cv;
 
+RotatedRect fitEllipse_edit( InputArray _points , double & error);
+CvBox2D cvFitEllipse_edit( const CvArr* array,double & error );
+
 int main(int argc, char * argv[])
 {
 	vector<Point2f> old_centers;
@@ -67,7 +70,7 @@ int main(int argc, char * argv[])
 				cvtColor(equalizedbgr_image, grey_image2, CV_BGR2GRAY);
 				GaussianBlur( grey_image2, blur_image2, Size(25,25),3, 3);
 				Canny( blur_image2, detected_edges, 10, 100, 3 );
-				imshow("after", detected_edges);
+
 				vector<vector<Point> > contours2;
 				findContours( detected_edges, contours2, hierarchy2, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_TC89_L1, Point(0, 0) );
 		
@@ -78,32 +81,22 @@ int main(int argc, char * argv[])
 				{
 					if(contours2[i].size() >=5){
 						//find ellipse
-						RotatedRect rec = fitEllipse(contours2[i]);
-						ellipse(bgr_image2, rec, Scalar(0,200,0), 3,8);
-						//get parameters for traversing ellipse
-						double theta = CvBox2D(rec).angle;
-						Point center = CvBox2D(rec).center;
-						int width = CvBox2D(rec).size.width;
-						int height = CvBox2D(rec).size.height;
-						//for each point in the ellipse, check 8 neighbors for being points in any curve
-						for(int j = 0; j < bgr_image.rows;j++)
-							for(int k = 0; k < bgr_image.cols;k++)
-							{	
-								double a = (j - center.x) * cos(theta) + (k - center.y) * sin(theta);
-								double b = (j - center.x) * sin(theta) - (k - center.y) * cos(theta);
-								double c = (a * a / width / width + b * b / height / height);
-								if(c < 1.1 || c > .9)
-								circle(bgr_image2, Point(j,k), 2, Scalar(200, 0 ,0 ) , 2, 8,0);
-							}
-						
+						double error;
+						RotatedRect rec = fitEllipse_edit(contours2[i], error);
+						if(abs(rec.size.width - rec.size.height) < .1 * rec.size.width)
+						{	
+							cout << error << " " << contours2[i].size() << " " << error / contours2[i].size()<<endl;
+							ellipse(bgr_image2, rec, Scalar(0,200,0), 3,8);
+						}
+
 					}
 				}
 				//find ellipse with most points in a vicinity
-			
+				imshow("1",bgr_image2);
 			
 
-				imshow("1",bgr_image2);
-				waitKey(-1);
+
+
 
 
 /*****************************/
@@ -197,11 +190,187 @@ int main(int argc, char * argv[])
 				centers_oldage.swap(centers_newage);
 
 
-				//imshow( "Mars2", bgr_image);	
+				imshow( "Mars2", bgr_image);	
   			}
 			key = waitKey(1);
 	}
 	return 0;
+}
+
+RotatedRect fitEllipse_edit( InputArray _points , double & error)
+{
+    Mat points = _points.getMat();
+    CV_Assert(points.checkVector(2) >= 0 &&
+              (points.depth() == CV_32F || points.depth() == CV_32S));
+    CvMat _cpoints = points;
+    return cvFitEllipse_edit(&_cpoints, error);
+}
+
+
+CvBox2D cvFitEllipse_edit( const CvArr* array , double & error)
+{
+    CvBox2D box;
+    cv::AutoBuffer<double> Ad, bd;
+    memset( &box, 0, sizeof(box));
+
+    CvContour contour_header;
+    CvSeq* ptseq = 0;
+    CvSeqBlock block;
+    int n;
+
+    if( CV_IS_SEQ( array ))
+    {
+        ptseq = (CvSeq*)array;
+        if( !CV_IS_SEQ_POINT_SET( ptseq ))
+            CV_Error( CV_StsBadArg, "Unsupported sequence type" );
+    }
+    else
+    {
+        ptseq = cvPointSeqFromMat(CV_SEQ_KIND_GENERIC, array, &contour_header, &block);
+    }
+
+    n = ptseq->total;
+    if( n < 5 )
+        CV_Error( CV_StsBadSize, "Number of points should be >= 5" );
+
+    /*
+     *  New fitellipse algorithm, contributed by Dr. Daniel Weiss
+     */
+    CvPoint2D32f c = {0,0};
+    double gfp[5], rp[5], t;
+    CvMat A, b, x;
+    const double min_eps = 1e-8;
+    int i, is_float;
+    CvSeqReader reader;
+
+    Ad.allocate(n*5);
+    bd.allocate(n);
+
+    // first fit for parameters A - E
+    A = cvMat( n, 5, CV_64F, Ad );
+    b = cvMat( n, 1, CV_64F, bd );
+    x = cvMat( 5, 1, CV_64F, gfp );
+
+    cvStartReadSeq( ptseq, &reader );
+    is_float = CV_SEQ_ELTYPE(ptseq) == CV_32FC2;
+
+    for( i = 0; i < n; i++ )
+    {
+        CvPoint2D32f p;
+        if( is_float )
+            p = *(CvPoint2D32f*)(reader.ptr);
+        else
+        {
+            p.x = (float)((int*)reader.ptr)[0];
+            p.y = (float)((int*)reader.ptr)[1];
+        }
+        CV_NEXT_SEQ_ELEM( sizeof(p), reader );
+        c.x += p.x;
+        c.y += p.y;
+    }
+    c.x /= n;
+    c.y /= n;
+
+    for( i = 0; i < n; i++ )
+    {
+        CvPoint2D32f p;
+        if( is_float )
+            p = *(CvPoint2D32f*)(reader.ptr);
+        else
+        {
+            p.x = (float)((int*)reader.ptr)[0];
+            p.y = (float)((int*)reader.ptr)[1];
+        }
+        CV_NEXT_SEQ_ELEM( sizeof(p), reader );
+        p.x -= c.x;
+        p.y -= c.y;
+
+        bd[i] = 10000.0; // 1.0?
+        Ad[i*5] = -(double)p.x * p.x; // A - C signs inverted as proposed by APP
+        Ad[i*5 + 1] = -(double)p.y * p.y;
+        Ad[i*5 + 2] = -(double)p.x * p.y;
+        Ad[i*5 + 3] = p.x;
+        Ad[i*5 + 4] = p.y;
+    }
+
+    cvSolve( &A, &b, &x, CV_SVD );
+
+    // now use general-form parameters A - E to find the ellipse center:
+    // differentiate general form wrt x/y to get two equations for cx and cy
+    A = cvMat( 2, 2, CV_64F, Ad );
+    b = cvMat( 2, 1, CV_64F, bd );
+    x = cvMat( 2, 1, CV_64F, rp );
+    Ad[0] = 2 * gfp[0];
+    Ad[1] = Ad[2] = gfp[2];
+    Ad[3] = 2 * gfp[1];
+    bd[0] = gfp[3];
+    bd[1] = gfp[4];
+    cvSolve( &A, &b, &x, CV_SVD );
+
+    // re-fit for parameters A - C with those center coordinates
+    A = cvMat( n, 3, CV_64F, Ad );
+    b = cvMat( n, 1, CV_64F, bd );
+    x = cvMat( 3, 1, CV_64F, gfp );
+    for( i = 0; i < n; i++ )
+    {
+
+        CvPoint2D32f p;
+        if( is_float )
+            p = *(CvPoint2D32f*)(reader.ptr);
+        else
+        {
+            p.x = (float)((int*)reader.ptr)[0];
+            p.y = (float)((int*)reader.ptr)[1];
+        }
+        CV_NEXT_SEQ_ELEM( sizeof(p), reader );
+        p.x -= c.x;
+        p.y -= c.y;
+        bd[i] = 1.0;
+        Ad[i * 3] = (p.x - rp[0]) * (p.x - rp[0]);
+        Ad[i * 3 + 1] = (p.y - rp[1]) * (p.y - rp[1]);
+        Ad[i * 3 + 2] = (p.x - rp[0]) * (p.y - rp[1]);
+    }
+    cvSolve(&A, &b, &x, CV_SVD);
+
+
+	const CvMat* A_const = &A;
+	const CvMat* b_const = 	&b;
+	const CvMat* x_const = &x;
+	Mat A_edit(A_const,false);
+	Mat x_edit(x_const,false);
+	Mat b_edit(b_const,false);
+   error = norm(A_edit * x_edit - b_edit);
+
+    // store angle and radii
+    rp[4] = -0.5 * atan2(gfp[2], gfp[1] - gfp[0]); // convert from APP angle usage
+    t = sin(-2.0 * rp[4]);
+    if( fabs(t) > fabs(gfp[2])*min_eps )
+        t = gfp[2]/t;
+    else
+        t = gfp[1] - gfp[0];
+    rp[2] = fabs(gfp[0] + gfp[1] - t);
+    if( rp[2] > min_eps )
+        rp[2] = sqrt(2.0 / rp[2]);
+    rp[3] = fabs(gfp[0] + gfp[1] + t);
+    if( rp[3] > min_eps )
+        rp[3] = sqrt(2.0 / rp[3]);
+
+    box.center.x = (float)rp[0] + c.x;
+    box.center.y = (float)rp[1] + c.y;
+    box.size.width = (float)(rp[2]*2);
+    box.size.height = (float)(rp[3]*2);
+    if( box.size.width > box.size.height )
+    {
+        float tmp;
+        CV_SWAP( box.size.width, box.size.height, tmp );
+        box.angle = (float)(90 + rp[4]*180/CV_PI);
+    }
+    if( box.angle < -180 )
+        box.angle += 360;
+    if( box.angle > 360 )
+        box.angle -= 360;
+
+    return box;
 }
 
 
